b6bd22d73cc333fbdea3f43fac399cea6039b6b0
Daryl <daryl.mathison@gmail.com>
2015-04-12 21:43:40 -0500

    Converted to Maven project

--- src/test/java/org/mathison/util/TestJarsSearch.java
+++ src/test/java/org/mathison/util/TestJarsSearch.java
@@ -1,30 +1,33 @@
-package org.mathison.util.test;
+package org.mathison.util;
+
+import junit.framework.TestCase;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.mathison.util.JarsSearch;
-
-import junit.framework.TestCase;
-
 public class TestJarsSearch extends TestCase {
 
-	public static final String ROOT_PATH = "classes/org/mathison/util/test";
+	public static final String ROOT_PATH = "./target/test-classes";
 	public static final File TEST2_JAR_FILE = new File(ROOT_PATH, "jars/subJars/test2.jar");
 	public static final File TEST_JAR_FILE = new File(ROOT_PATH, "jars/alltest.jar");
 	
 	public void testFind() {
 		try {
-			
+			assertTrue(TEST2_JAR_FILE.exists());
+			assertTrue(TEST_JAR_FILE.exists());
+
 			JarsSearch js = new JarsSearch();
 			js.setRootpath(ROOT_PATH);
 			js.setFilename("test.*");
 			
 			String jarName = js.find();
+			System.out.println("jarname1 - > " + jarName);
+			System.out.println("TEST2_JAR_FILE ->" + TEST2_JAR_FILE.getCanonicalPath());
 			assertEquals(TEST2_JAR_FILE.getCanonicalPath(), jarName);
 			
 			js.setFilename("lib.*");
 			jarName = js.find();
+			System.out.println("jarName2 -> " + jarName);
 			assertEquals(TEST_JAR_FILE.getCanonicalPath(), jarName);
 		} catch (IOException e) {
 			e.printStackTrace();
